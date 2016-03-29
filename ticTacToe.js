93e307d5124851ca1bf4a2ff7db0370fccb8fa1b
Akiva Szental <amszental@gmail.com>
2016-03-29 16:23:26 +1100

    alternates x and y on click

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,14 +1,27 @@
-var clickedSquare = document.querySelector('.square')
 var board = document.querySelector('.board')
+var currentPlayer = 0
 
-var id = 0
+var playerOne = function (event) {
+  event.target.style.backgroundColor = 'green'
+}
+
+var playerTwo = function (event) {
+  event.target.style.backgroundColor = 'blue'
+}
+
+board.addEventListener('click', turn);
 
 
+function turn(event) {
+  console.log('clicked')
 
-board.addEventListener('click', function (event)
-  {
+  if (currentPlayer === 0) {
+    event.target.innerHTML = 'X'
+    currentPlayer = 1
 
-    var clickedSquareId = event.target.id;
-    event.target.style.backgroundColor = "red";
   }
-)
+  else if (currentPlayer === 1) {
+    event.target.innerHTML = 'Y'
+    currentPlayer = 0
+  }
+}
