f3bcafdc2007aedcae7d5a06a12877b6a0ed2577
Akiva Szental <amszental@gmail.com>
2016-03-30 11:38:09 +1100

    added tie check

--- ticTacToe.js
+++ ticTacToe.js
@@ -6,6 +6,7 @@ var winConditions = [[11, 12, 13], [21, 22, 23], [31, 22, 33], [11, 21,31], [12,
 var xMoves = []
 var yMoves = []
 var isWinner = false;
+var isTie = false;
 
 
 // var playerOne = function (event) {
@@ -24,15 +25,14 @@ board.addEventListener('click', turn);
 
 
 function turn(event) {
-  if (event.target.className === 'square') {
+  if (event.target.className === 'square') { // check if class has already been applied
 
     if (currentPlayer === 'X') {
-      event.target.className = 'x';
-      xMoves.push(parseInt(event.target.id));
-      winChecker(xMoves)
-      currentPlayer = 'Y';
+      event.target.className = 'x'; // change square to x class
+      xMoves.push(parseInt(event.target.id)); //push to moves array
+      winChecker(xMoves) // see if x is winner
+      currentPlayer = 'Y'; // set player to y
       document.getElementById('current-player').innerHTML = "Y"
-
 
   }
 
@@ -46,33 +46,42 @@ function turn(event) {
   }
 
  }
-  else {
-    return false;
-  }
 }
 
 
 function winChecker(moves) {
-for (var j = 0; j < winConditions.length; j++) {
-	var matches = [];
-	for (var i = 0; i < moves.length; i++) {
+for (var j = 0; j < winConditions.length; j++) { //loop through win array
+	var matches = [];  //empty array to hold matches
+	for (var i = 0; i < moves.length; i++) { //loops through the moves
 				var match = false;
-		for (var x = 0; x < winConditions[j].length; x++ ) {
-			if (moves[i] === winConditions[j][x]) {
+		for (var x = 0; x < winConditions[j].length; x++ ) //loops through individual wins in the wins array
+     {
+			if (moves[i] === winConditions[j][x]) // compares the moves to individual win array
+       {
 				match = true;
-				matches.push(match)
-        console.log(matches)
+				matches.push(match) //push the true to the array.
 			}
 		}
 	}
-	if (matches.length === 3) {
+	if (matches.length === 3) //if found 3 matches there is a winner
+
+  {
 
 	 isWinner = true;
+	}
+
+  if (xMoves.length + yMoves.length === 9) //if 9 moves been a tie
+  {
+	 isTie = true;
 	}
 
 }
   if(isWinner) {
     window.alert(currentPlayer + " Is the winner!")
+    boardReset();
+  }
+  if(isTie) {
+    window.alert("It is a Tie!")
     boardReset();
   }
    }
