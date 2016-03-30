f1d0c6c779ed4998b5df6e138be0becc396d78ce
Akiva Szental <amszental@gmail.com>
2016-03-30 11:17:12 +1100

    working game with board reset

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,8 @@
 var board = document.querySelector('.board')
 var playerSpan = document.getElementById('current-player')
-var currentPlayer = 0
+var currentPlayer = 'X'
 
-var winConditions = [[11, 12, 13], [11, 21, 23], [21, 22, 23], [31, 32,33], [21, 22, 23],[31, 32, 33], [11, 22, 23], [13, 22, 31],[13, 23, 33]]
+var winConditions = [[11, 12, 13], [21, 22, 23], [31, 22, 33], [11, 21,31], [12, 22, 32],[13, 23, 33], [11, 22, 33], [13, 22, 31]]
 var xMoves = []
 var yMoves = []
 var isWinner = false;
@@ -24,21 +24,25 @@ board.addEventListener('click', turn);
 
 
 function turn(event) {
-  console.log(event.target.className)
   if (event.target.className === 'square') {
 
-    if (currentPlayer === 0) {
+    if (currentPlayer === 'X') {
       event.target.className = 'x';
-      currentPlayer = 1;
       xMoves.push(parseInt(event.target.id));
       winChecker(xMoves)
+      currentPlayer = 'Y';
+      document.getElementById('current-player').innerHTML = "Y"
+
+
   }
 
-    else if (currentPlayer === 1) {
+    else if (currentPlayer === 'Y') {
       event.target.className = 'y';
-      currentPlayer = 0;
       yMoves.push(parseInt(event.target.id));
       winChecker(yMoves)
+      currentPlayer = 'X';
+      document.getElementById('current-player').innerHTML = "X"
+
   }
 
  }
@@ -57,16 +61,34 @@ for (var j = 0; j < winConditions.length; j++) {
 			if (moves[i] === winConditions[j][x]) {
 				match = true;
 				matches.push(match)
+        console.log(matches)
 			}
 		}
 	}
-
 	if (matches.length === 3) {
+
 	 isWinner = true;
 	}
 
 }
   if(isWinner) {
     window.alert(currentPlayer + " Is the winner!")
+    boardReset();
   }
    }
+
+function boardReset() {
+  xMoves = []
+  yMoves = []
+  isWinner = false;
+  var elsX = document.getElementsByClassName('x');
+  var elsY = document.getElementsByClassName('y');
+
+  while (elsX.length) {
+    elsX[0].className = 'square';
+}
+while (elsY.length) {
+  elsY[0].className = 'square';
+
+}
+}
