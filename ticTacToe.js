196ec176b7e6adc4d14362278d881e4ef3bfef90
Akiva Szental <amszental@gmail.com>
2016-03-31 21:10:05 +1100

    added delay

--- ticTacToe.js
+++ ticTacToe.js
@@ -190,7 +190,7 @@ function computerMove() {
 }
 
 
-$(".youtube").delay( 1200 ).colorbox({iframe:true, innerWidth:640, innerHeight:390});
+$(".youtube").delay( 3000 ).colorbox({iframe:true, innerWidth:640, innerHeight:390});
 function trumpWins() {
-  $('.youtube').click();
+  $('.youtube').delay( 3000 ).click();
 }
